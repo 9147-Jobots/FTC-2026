190dc609215ec5a936da592e06244309d5612fc2
Timothy Shu <tims11105@gmail.com>
2026-09-14 17:29:15 +1000

    added field relative code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.framework.SubsystemBase;
 
 /**
@@ -10,6 +14,8 @@ import org.firstinspires.ftc.teamcode.framework.SubsystemBase;
  */
 public class DriveSubsystem extends SubsystemBase {
     private final Telemetry telemetry;
+
+    private final IMU imu;
 
     private final DcMotor frontLeftDrive;
     private final DcMotor backLeftDrive;
@@ -23,6 +29,16 @@ public class DriveSubsystem extends SubsystemBase {
         this.telemetry = telemetry;
 
         // Perform hardware initializations directly inside the constructor
+
+        // IMU
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+        imu.initialize(parameters);
+
+        // Drive
         frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
         backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
         frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
@@ -43,10 +59,16 @@ public class DriveSubsystem extends SubsystemBase {
      * Controls the wheels given directional power components.
      */
     public void drive(double axial, double lateral, double yaw) {
-        double frontLeftPower  = axial + lateral + yaw;
-        double frontRightPower = axial - lateral - yaw;
-        double backLeftPower   = axial - lateral + yaw;
-        double backRightPower  = axial + lateral - yaw;
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        double rotX = lateral * Math.cos(-botHeading) - axial * Math.sin(-botHeading);
+        double rotY = lateral * Math.sin(-botHeading) + axial * Math.cos(-botHeading);
+
+        double frontLeftPower  = rotY + rotX + yaw;
+        double frontRightPower = rotY - rotX - yaw;
+        double backLeftPower   = rotY - rotX + yaw;
+        double backRightPower  = rotY + rotX - yaw;
 
         double max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
         max = Math.max(max, Math.abs(backLeftPower));
@@ -66,5 +88,9 @@ public class DriveSubsystem extends SubsystemBase {
 
         telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
         telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
+    }
+
+    public void resetHeading() {
+        imu.resetYaw();
     }
 }
